b24e71ed44bd6f0840e767d252a7f8ec6d08943c
Elias Duvernois <e.duvernois2016@gmail.com>
2023-04-24 16:12:28 +0200

    test

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const open = document.getElementById("open");
 const close = document.getElementById("close");
 const navbar = document.getElementById("navbar")
+const a = 0;
 
 open.addEventListener("click", ()=> {
     navbar.classList.remove("-translate-x-full");
