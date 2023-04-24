8db0044b9cd6145f57d79dc4db7e853253f956a7
Elias Duvernois <e.duvernois2016@gmail.com>
2023-04-24 15:18:22 +0200

    intialisation du repository

---
+++ js/index.js
@@ -0,0 +1,11 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const navbar = document.getElementById("navbar");
+
+open.addEventListener("click", ()=> {
+    navbar.classList.remove("-translate-x-full");
+})
+
+close.addEventListener("click", ()=> {
+    navbar.classList.add("-translate-x-full");
+})
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'greenD' : "#3D550C",
+        'greenYellow' : "#ECF87F",
+        'greenB' : "#81B622"
+      },
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        'regular' : 400,
+        'semibold': 600,
+        'bold': 700,
+      },
+      backgroundImage: {
+        "cs": "url(../assets/img/informatique.jpg)"
+      },
+      textDecorationThickness:{
+        "underline-offset-thick": "8px"
+      },
+      width: {
+        "max-w-imgM":"50%"
+      },
+      screens: {
+        'ssm': '410px'
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
